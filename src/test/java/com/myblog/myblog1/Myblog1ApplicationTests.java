856b0bb949fd7043a34ee35b37e98aaea92ee3e1
sandarv <varshayadav13077@gmail.com>
2024-01-04 16:56:28 +0530

    127:Create Save Post Feature

---
+++ src/test/java/com/myblog/myblog1/Myblog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
